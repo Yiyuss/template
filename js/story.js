5aa26cbc57e9ada092c55e284b5febb609e85c68
Yiyuss <155512187+Yiyuss@users.noreply.github.com>
2025-09-21 21:08:47 +0800

    Update story.js

--- js/story.js
+++ js/story.js
@@ -7,7 +7,7 @@ const Story = {
     scenes: {
         // 開始場景
         start: {
-            background: 'assets/images/backgrounds/bg1.jpg',
+            background: 'assets/images/backgrounds/bg1.gif',
             bgm: 'assets/audio/start_bgm.mp3', // 可選，場景特定的BGM
             dialogues: [
                 {
@@ -203,4 +203,5 @@ const Story = {
             }
         ]
     }
+
 };
