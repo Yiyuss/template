5602cc5eeac9e714d5f6e564b219ead6d417d657
Yiyuss <155512187+Yiyuss@users.noreply.github.com>
2025-09-22 14:25:05 +0800

    Update story.js

--- js/story.js
+++ js/story.js
@@ -12,33 +12,42 @@ const Story = {
             dialogues: [
                 {
                     character: '伊鬱',
-                    text: '初次讀取請耐心等候圖片生成，請點擊對話框繼續。',
-                    voice: 'assets/voice/start_1.mp3'
+                    text: '初次讀取請耐心等候圖片生成，請點擊對話框繼續。'
                 },
                 {
                     character: '伊鬱',
                     text: '歡迎來到花語·無聲！',
-                    voice: 'assets/voice/start_2.mp3'
+                    voice: 'assets/voice/start_1.mp3'
                 },
                 {
                     character: '伊鬱',
                     text: '一個叫做 埃露芙塔（Eluftha） 的世界，一切情感皆可產生魔力，稱為「情咒」。',
+                    voice: 'assets/voice/start_2.mp3'
+                },
+                {
+                    character: '伊鬱',
+                    text: '情咒不需要吟唱，情感到達某種閾值時，自然會轉化為「輕魔法」。',
                     voice: 'assets/voice/start_3.mp3'
                 },
                 {
                     character: '伊鬱',
-                    text: '情咒不需要吟唱，情感到達某種閾值時，自然會轉化為「輕魔法」——能微調物理現實（例如改變花的顏色、讓空氣中出現聲音幻象、投影記憶等），但無法直接用於戰鬥。',
+                    text: '——能微調物理現實（例如改變花的顏色、讓空氣中出現聲音幻象、投影記憶等），但無法直接用於戰鬥。',
                     voice: 'assets/voice/start_4.mp3'
+
                 },
                 {
                     character: '伊鬱',
-                    text: '政府將情咒分級管理，過度的情感會被視為「危咒」。於是，越是壓抑感情的人，越容易擁有強大的情咒潛力。',
+                    text: '政府將情咒分級管理，過度的情感會被視為「危咒」。',
                     voice: 'assets/voice/start_5.mp3'
                 },
                 {
                     character: '伊鬱',
+                    text: '於是，越是壓抑感情的人，越容易擁有強大的情咒潛力。',
+                    voice: 'assets/voice/start_6.mp3'
+                },
+                {
+                    character: '伊鬱',
                     text: '請選擇妳想成為的角色：',
-                    voice: 'assets/voice/start_6.mp3',
                     choices: [
                         {
                             text: '瑪格麗特·諾爾絲',
